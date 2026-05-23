fed59b86064c2d9e878b24b4c450f7bbfef31131
scminy0527-rgb <scminy0527@gmail.com>
2026-05-23 16:54:34 +0900

    *fix: 커뮤니티 메인 게시글 카테고리 선택 버튼 PostListSection.jsx 로 이동

--- src/pages/community/post/CommunityPostContainer.jsx
+++ src/pages/community/post/CommunityPostContainer.jsx
@@ -1,84 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { ColumnBlock, ActionBtn, CategoryPill } from "../communityStyle";
+import { ColumnBlock, ActionBtn } from "../communityStyle";
 import T from "../communityTextStyle";
 import PostListSection from "./postComponents/PostListSection.jsx";
 import LiveChatListSection from "./postComponents/LiveChatListSection.jsx";
 import { useChatContext } from "../context/ChatContext";
-import { POST_CATEGORIES } from "../constants";
 import {
   HeaderBlock,
-  PostHeader,
-  PostCategoryHeader,
-  PostCategoryRow,
   AllChatButton,
 } from "./communityPostContainerStyle";
 
 const S = {
   ColumnBlock,
   ActionBtn,
-  CategoryPill,
   HeaderBlock,
-  PostHeader,
-  PostCategoryHeader,
-  PostCategoryRow,
   AllChatButton,
 };
 
-// 컴포넌트
 const CommunityPostContainer = () => {
   const { openCreateChatRoom } = useChatContext();
-  const [selectedTag, setSelectedTag] = useState("");
 
   console.log("메인 영역 그려지기");
   return (
     <div>
-      {/* <CommunityPostComponent /> */}
       <S.ColumnBlock>
-        {/* 상단 헤더 */}
         <S.HeaderBlock>
-          {/* 제목 */}
           <T.H6Bold>실시간 채팅방</T.H6Bold>
-
-          {/* 글쓰기 버튼 */}
           <S.ActionBtn $type="submit" onClick={openCreateChatRoom}>
             +채팅방 만들기
           </S.ActionBtn>
         </S.HeaderBlock>
-        {/* 채팅방 */}
         <LiveChatListSection />
-        {/* 채팅방 모두 보기 버튼 */}
         <S.AllChatButton>
           <Link to={"/community/chat"}>전체 보기 →</Link>
         </S.AllChatButton>
-        {/* 포스트 영역 헤더 */}
-        <S.PostHeader>
-          <T.H7Bold>게시글</T.H7Bold>
-        </S.PostHeader>
-        {/* 카테고리 및 글쓰기 버튼 */}
-        <S.PostCategoryHeader>
-          {/* 카테고리 */}
-          <S.PostCategoryRow>
-            {POST_CATEGORIES.map(({ label, value }) => (
-              <S.CategoryPill
-                key={value}
-                $active={selectedTag === value}
-                onClick={() => setSelectedTag(value)}
-              >
-                {label}
-              </S.CategoryPill>
-            ))}
-          </S.PostCategoryRow>
-          <Link to="/community/post/write">
-            <S.ActionBtn $type="submit">글쓰기</S.ActionBtn>
-          </Link>
-
-          {/* 글쓰기 */}
-        </S.PostCategoryHeader>
-        {/* 포스트 카드 목록 + 페이지네이션 */}
-        <PostListSection postTag={selectedTag} />
-
-        {/* 테스트 */}
+        <PostListSection />
       </S.ColumnBlock>
     </div>
   );
--- src/pages/community/post/postComponents/PostListSection.jsx
+++ src/pages/community/post/postComponents/PostListSection.jsx
@@ -1,31 +1,46 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import PostListCard from "./PostListCard.jsx";
 import PageCount from "./PageCount";
 import { fetchPosts } from "../../communityApi/postApi";
-import { ColumnBlock } from "../../communityStyle";
+import { ColumnBlock, CategoryPill, ActionBtn } from "../../communityStyle";
 import PostListCardSkeleton from "../skeleton/PostListCardSkeleton.jsx";
+import { POST_CATEGORIES } from "../../constants";
+import T from "../../communityTextStyle";
+import {
+  PostHeader,
+  PostCategoryHeader,
+  PostCategoryRow,
+} from "../communityPostContainerStyle";
 
 const S = {
   ColumnBlock,
+  CategoryPill,
+  ActionBtn,
+  PostHeader,
+  PostCategoryHeader,
+  PostCategoryRow,
 };
 
-const PostListSection = ({ postTag = "" }) => {
+const PostListSection = () => {
+  const [selectedTag, setSelectedTag] = useState("");
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
 
-  // const { userId } = useParams({ userId });
-
   useEffect(() => {
     setCurrentPage(1);
-  }, [postTag]);
+  }, [selectedTag]);
 
   useEffect(() => {
     const load = async () => {
       setIsLoading(true);
       try {
-        const res = await fetchPosts({ page: currentPage, postTag });
+        const res = await fetchPosts({
+          page: currentPage,
+          postTag: selectedTag,
+        });
         setPosts(res.data.posts);
         setTotalPages(res.data.totalPages);
       } catch (e) {
@@ -35,61 +50,81 @@ const PostListSection = ({ postTag = "" }) => {
       }
     };
     load();
-  }, [currentPage, postTag]);
+  }, [currentPage, selectedTag]);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  if (isLoading)
-    return (
-      <S.ColumnBlock>
-        <PostListCardSkeleton />
-        <PostListCardSkeleton />
-        <PostListCardSkeleton />
-        <PostListCardSkeleton />
-      </S.ColumnBlock>
-    );
-
   return (
-    <S.ColumnBlock marginBottom="42px">
-      {posts.map(
-        ({
-          id,
-          postTag,
-          postCreateAt,
-          postTitle,
-          postContent,
-          userNickname,
-          userProfile,
-          likeCount,
-          commentCount,
-          postReadCount,
-        }) => (
-          <PostListCard
-            key={id}
-            id={id}
-            postTag={postTag}
-            postCreateAt={postCreateAt}
-            postTitle={postTitle}
-            postContent={postContent}
-            userNickname={userNickname}
-            userProfile={userProfile}
-            likeCount={likeCount}
-            commentCount={commentCount}
-            postReadCount={postReadCount}
-          />
-        ),
+    <>
+      <S.PostHeader>
+        <T.H7Bold>게시글</T.H7Bold>
+      </S.PostHeader>
+      <S.PostCategoryHeader>
+        <S.PostCategoryRow>
+          {POST_CATEGORIES.map(({ label, value }) => (
+            <S.CategoryPill
+              key={value}
+              $active={selectedTag === value}
+              onClick={() => setSelectedTag(value)}
+            >
+              {label}
+            </S.CategoryPill>
+          ))}
+        </S.PostCategoryRow>
+        <Link to="/community/post/write">
+          <S.ActionBtn $type="submit">글쓰기</S.ActionBtn>
+        </Link>
+      </S.PostCategoryHeader>
+      {isLoading ? (
+        <S.ColumnBlock>
+          <PostListCardSkeleton />
+          <PostListCardSkeleton />
+          <PostListCardSkeleton />
+          <PostListCardSkeleton />
+        </S.ColumnBlock>
+      ) : (
+        <S.ColumnBlock marginBottom="42px">
+          {posts.map(
+            ({
+              id,
+              postTag,
+              postCreateAt,
+              postTitle,
+              postContent,
+              userNickname,
+              userProfile,
+              likeCount,
+              commentCount,
+              postReadCount,
+            }) => (
+              <PostListCard
+                key={id}
+                id={id}
+                postTag={postTag}
+                postCreateAt={postCreateAt}
+                postTitle={postTitle}
+                postContent={postContent}
+                userNickname={userNickname}
+                userProfile={userProfile}
+                likeCount={likeCount}
+                commentCount={commentCount}
+                postReadCount={postReadCount}
+              />
+            ),
+          )}
+          {totalPages > 1 && (
+            <PageCount
+              totalPages={totalPages}
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+            />
+          )}
+        </S.ColumnBlock>
       )}
-      {totalPages > 1 && (
-        <PageCount
-          totalPages={totalPages}
-          currentPage={currentPage}
-          onPageChange={handlePageChange}
-        />
-      )}
-    </S.ColumnBlock>
+    </>
   );
 };
 
